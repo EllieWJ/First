2be1d6b485a8e39f8b34c90b026caa704dfeca87
EllieWJ <1521754951@qq.com>
2021-09-27 23:00:52 +0800

    修改注释

---
+++ app/src/main/java/com/swufestu/hello/MainActivity_rmb.java
@@ -0,0 +1,112 @@
+package com.swufestu.hello;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+
+public class MainActivity_rmb extends AppCompatActivity {
+    private static final String TAG="MainActivity_rmb";
+    private float dollarrate=0.1548f;
+    private float eurorate=0.1323f;
+    private float wonrate=182.5773f;
+    EditText rmb;
+    TextView output1;
+    double num;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rmb);
+        rmb=findViewById(R.id.rmb);
+        output1=findViewById(R.id.outnum);
+
+
+    }
+    public void click(View btn){
+        Log.i(TAG,"click:");
+        float temp;
+        String rmba=rmb.getText().toString();
+        if(rmba.length()>0){
+            temp=Float.parseFloat(rmba);
+            double result=0.0;
+            if(btn.getId()==R.id.DOLLAR){
+                result=temp*dollarrate;
+            }else if(btn.getId()==R.id.EURO){
+                result=temp*eurorate;
+            }else if(btn.getId()==R.id.WON){
+                result=temp*wonrate;
+            }else{
+                result=0.0;
+            }
+            output1.setText(String.valueOf(result));
+        }else{
+            //重置输出
+            output1.setText("Hello");
+            Toast.makeText(this, "请输入金额后再进行计算", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void openconfig(View v){
+        //打开窗口
+        Log.i(TAG,"openconfig:");
+        config();
+    }
+
+    private void config() {
+        Intent config = new Intent(this, MainActivity_rmb2.class);
+        config.putExtra("dollar_rate_key", dollarrate);
+        config.putExtra("euro_rate_key", eurorate);
+        config.putExtra("won_rate_key", wonrate);
+
+        Log.i(TAG, "openone:dollarrate=" + dollarrate);
+        Log.i(TAG, "openone:eurorate=" + eurorate);
+        Log.i(TAG, "openone:wonrate=" + wonrate);
+
+        //startActivity(config);
+        startActivityForResult(config, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1&&resultCode==3){
+            /**
+            dollarrate=data.getFloatExtra("key_dollar",0.1f);
+            eurorate=data.getFloatExtra("key_euro",0.1f);
+            wonrate=data.getFloatExtra("key_won",0.1f); **/
+            Bundle bundle=new Bundle();
+            dollarrate=bundle.getFloat("key_dollar",0.1f);
+            eurorate=bundle.getFloat("key_euro",0.1f);
+            wonrate=bundle.getFloat("key_won",0.1f);
+            Log.i(TAG,"onActivityResult:dollarrate="+dollarrate);
+            Log.i(TAG,"onActivityResult:eurorate="+eurorate);
+            Log.i(TAG,"onActivityResult:wonrate="+wonrate);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rate,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.menu_set){
+            config();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/swufestu/hello/MainActivity_rmb2.java
@@ -0,0 +1,64 @@
+package com.swufestu.hello;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity_rmb2 extends AppCompatActivity {
+    private static final String TAG="MainActivity_rmb2";
+    private EditText output1,output2,output3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_rmb2);
+
+        Intent intent=getIntent();
+        float dollar=intent.getFloatExtra("dollar_rate_key",0.0f);
+        float euro=intent.getFloatExtra("euro_rate_key",0.0f);
+        float won=intent.getFloatExtra("won_rate_key",0.0f);
+
+        Log.i(TAG,"openone:dollar="+dollar);
+        Log.i(TAG,"openone:euro="+euro);
+        Log.i(TAG,"openone:won="+won);
+
+        output1=findViewById(R.id.input1d);
+        output2=findViewById(R.id.input2e);
+        output3=findViewById(R.id.input3w);
+        //将汇率填入到控件中
+        output1.setText(String.valueOf(dollar));
+        output2.setText(String.valueOf(euro));
+        output3.setText(String.valueOf(won));
+    }
+
+    public void opensave(View v){
+        Log.i(TAG,"opensave:");
+        float dollar=Float.parseFloat(output1.getText().toString());
+        float euro=Float.parseFloat(output2.getText().toString());
+        float won=Float.parseFloat(output3.getText().toString());
+
+        Log.i(TAG,"opensave:dollarrate="+dollar);
+        Log.i(TAG,"opensave:eurorate="+euro);
+        Log.i(TAG,"opensave:wonrate="+won);
+
+        Intent save=getIntent();
+        Bundle bdl=new Bundle();
+        bdl.putFloat("dollar_key",dollar);
+        bdl.putFloat("euro_key",euro);
+        bdl.putFloat("won_key",won);
+        save.putExtras(bdl);
+ /**
+        Intent save=getIntent();
+        save.putExtra("dollar_key",dollar);
+        save.putExtra("euro_key",euro);
+        save.putExtra("won_key",won);
+**/
+        //startActivity(save);
+        setResult(3,save);
+        finish();
+
+    }
+}
